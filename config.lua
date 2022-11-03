85d792e80d355bbe6c7a6561095fcc38228ff28c
FORCE <111786799+JustAbdulla@users.noreply.github.com>
2022-11-03 14:30:27 +0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,10 @@
 Config = {}
-Config.Interior = vector3(972.93481, 74.040916, 116.16419) -- Interior to load where characters are previewed
+Config.StartingApartment = true -- Enable/disable starting apartments (make sure to set default spawn coords)
+Config.Interior = vector3(-8.88, 512.91, 174.63) -- Interior to load where characters are previewed
 Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
-Config.PedCoords = vector4(967.48022, 73.072288, 116.17608, 275.25238) -- Create preview ped at these coordinates
-Config.HiddenCoords = vector4(975.12506, 72.92913, 116.1641, 258.05444) -- Hides your actual ped while you are in selection
-Config.CamCoords = vector4(972.57196, 73.8675, 116.16419, 100.03983) -- Camera coordinates for character preview screen
+Config.PedCoords = vector4(-8.55, 508.84, 174.63, 19.81) -- Create preview ped at these coordinates
+Config.HiddenCoords = vector4(3.3, 523.88, 170.62, 70.87) -- Hides your actual ped Ma-cro guapo while you are in selection
+Config.CamCoords = vector4(-8.88, 512.91, 174.63, 185.5) -- Camera coordinates for character preview screen 
 Config.EnableDeleteButton = true -- Define if the player can delete the character or not
 
 Config.DefaultNumberOfCharacters = 5 -- Define maximum amount of default characters (maximum 5 characters defined by default)
